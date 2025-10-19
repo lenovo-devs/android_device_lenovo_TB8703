4fc639d45dbe9e02bd351ae048128477b656f87a
Vitaliy Tomin <highwaystar.ru@gmail.com>
2025-10-20 00:11:10 +0800

    TB8703: init: Drop Android.mk
    Change-Id: I3099cee1846ef7200d567670b8ec0ed956a8ce31

--- init/init_lenovo_tb8703.cpp
+++ init/init_lenovo_tb8703.cpp
@@ -32,10 +32,6 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 #include <stdio.h>
-#include <sys/system_properties.h>
-
-#include "vendor_init.h"
-#include "property_service.h"
 
 #define PROP_BOOT_BASEBAND "ro.boot.baseband"
 using android::base::GetProperty;
